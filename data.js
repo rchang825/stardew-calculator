3f4e9c9284ccdcb5cdef48f996ca86a0adfa3a4a
Ruby Chang <rchang825@hotmail.com>
2025-11-04 21:58:38 -0800

    dummy data

---
+++ data.js
@@ -0,0 +1,30 @@
+const crops = {
+  "Parsnip": {
+    "name": "Parsnip",
+    "daysToGrow": 4,
+    "regrowthTime": 0,
+    "seasons": ["summer"],
+    "sellPrice": 35,
+    "edible": true,
+    "season": "spring"
+  },
+  "Potato": {
+    "name": "Potato",
+    "daysToGrow": 6,
+    "regrowthTime": 0,
+    "seasons": ["spring"],
+    "sellPrice": 80,
+    "edible": true,
+    "season": "spring"
+  },
+  "Strawberry": {
+    "name": "Strawberry",
+    "daysToGrow": 8,
+    "regrowthTime": 4,
+    "seasons": ["spring"],
+    "sellPrice": 120,
+    "edible": true,
+    "season": "spring"
+  }
+};
+export default crops;
